f2f72c1ed7913d30b450c5cb79960c73e639776c
Sugarsplash <bryan3@pdx.edu>
2015-11-30 07:02:02 -0800

    Add pseudo code for write logic.

--- src/memControl.cpp
+++ src/memControl.cpp
@@ -15,13 +15,20 @@ M3 M3array[8];
 /*These are parts of the M1 memory reserved for controll memory for each 
   mem array
 */
-int M2controll_mem[16];
-int M3controll_mem[64];
+int M2control_mem[16];
+int M3control_mem[64];
+
+//Keep track of valid lines in M2controll_mem and M3controll_mem
+int M2valid_array[16];
+int M3valid_array[64];
 
 //Data Types
 #define WORD = 0
 #define	QUAD = 1
 #define	LONG = 2
+
+#define SEND = 0
+#define REQUEST = 1
 
 //Function to write 128byte word to M2 memory
 void M2word_write(int address, int data)
@@ -37,7 +44,7 @@ void M2word_write(int address, int data)
 	}
 }
 
-//Function to write 128byte word to M2 memory
+//Function to read 128byte word from M2 memory
 void M2word_read(int address, int readData[64])
 {
 	int bank, row;
@@ -67,7 +74,7 @@ void M3word_write(int address, int data)
 	}
 }
 
-//Function to write 128byte word to M3 memory
+//Function to read 128byte word from M3 memory
 void M3word_read(int address, int readData[64])
 {
 	int bank, row;
@@ -86,6 +93,47 @@ void M3word_read(int address, int readData[64])
 
 int main()
 {
+	device = 1;
+	op = SEND;
+	int ts = WORD; 
+	int tag = 12345;
+
+	if(op == SEND)
+	{
+		switch (ts)
+		{
+			case WORD:
+				//findFreeLine needs to find lowest zero in M1valid_array.
+				int writeLine = findFreeLine();
+				//M1generate needs to create the three byte line to store in M1.
+				M1generate(writeLine);
+				break;
+
+			case QUAD:
+				//Function M1generate4 needs to construct 4 M1 3 byte lines.
+				int writeLines[4];
+				for(int i=0; i < 3; i++)
+				{
+					writeLines[i] = findFreeLine();
+				}
+				M1generate4(writeLines);
+				break;
+
+			case LONG:
+				//Function M1generate8 needs to construct 8 M1 3 byte lines.
+				int writeLong[8];
+				for(int i=0; i < 7; i++)
+				{
+					writeLong[i] = findFreeLine();
+				}
+				M1generate8(writeLong);
+				break;
+		}
+	}
+
+
+
+
 	int address = 0;
 	int data = 0xDEAD;
 	int readData[64];
